4e6ed6fc5d79e8026a14af854617d7308f3a9d12
atai20 <37374053+atai20@users.noreply.github.com>
2020-11-04 02:53:42 +0300

    some changes

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ import pygame
 import math
 
 t = 0
-g = 9.8
 
+
+def vector_sum(obj, direct):
+    for d in range(len(direct)):
+        obj.v_vel = direct[d][1]
+
+        if direct[d][0] != 0:
+            obj.main_v = (direct[d][0], direct[d][1])
+        else:
+            obj.main_v = (2 * math.pi, direct[d][1])
+
+        obj.x += obj.v_vel * math.cos(direct[d][0])
+        obj.y += obj.v_vel * math.sin(direct[d][0])
 
 class Object:
     def __init__(self, x, y, m, wide):
@@ -11,7 +22,7 @@ class Object:
         self.y = y
         self.m = m
         self.wide = wide
-        self.main_F = (0, 0)
+        self.main_v = (0, 0)
         self.ground_y = 400
         self.obj = pygame.draw.rect(win, (255, 255, 255), (self.x, self.y, 50, 50))
         self.ground = pygame.draw.rect(win, (255, 255, 255), (0, self.ground_y, 500, 500))
@@ -35,16 +46,8 @@ class Objects():
         self.objects[index].obj = pygame.draw.rect(win, (255, 255, 255),
                                                    (self.objects[index].x, self.objects[index].y, 50, 50), 1)
 
-        for d in range(len(direct)):
-            self.objects[index].f = direct[d][1]
-            if direct[d][0]==0:
-                self.objects[index].main_F = (direct[d][0], direct[d][1])
-            else:
-                self.objects[index].main_F = (2*math.pi, direct[d][1])
 
-            self.objects[index].x += self.objects[index].f * math.cos(direct[d][0])
-            self.objects[index].y += self.objects[index].f * math.sin(direct[d][0])
-
+        vector_sum(self.objects[index], direct)
 
         self.objects[index].draw()
 
@@ -53,19 +56,20 @@ class Objects():
         for object in obj:
 
             object.index = self.objects.index(object)
-            object.Vg = (math.pi / 2, (t ** 2 * g) / 2)
-            object.Fgrav = (math.pi / 2, self.objects[object.index].m * g)
 
-            for objct1 in self.objects:
-                for objct2 in obj:
-                    if objct2.obj.colliderect(objct1.obj) and objct2 != objct1:
-                        if objct1.main_F[0]==objct2.main_F[0]:
-                            objct2.P = (objct2.main_F[0], (2*objct1.m * objct1.f + objct2.f*(objct2.m - objct1.m) )/objct2.m + objct1.m)
-                        else:
-                            objct2.P = (objct2.main_F[0]-math.pi, (2*objct1.m * objct1.f + objct2.f*(objct2.m - objct1.m) )/objct2.m + objct1.m)
 
-            print(str(obj[0].main_F[0])+' '+str(obj[0].m))
-            print(str(obj[1].main_F[0])+' '+str(obj[1].m))
+            for objct2 in self.objects:
+                for objct1 in obj:
+                    if objct1.obj.colliderect(objct2.obj) and objct1 != objct2:
+                        self.go_to(objct1, (2*objct2.main_v[0], 1))
+                        print(objct2.main_v[0])
+
+
+
+
+
+            print(str(obj[0].main_v[0])+' '+str(obj[0].m))
+            print(str(obj[1].main_v[0])+' '+str(obj[1].m))
 
 
             self.go_to(object, (0, 0))
@@ -92,8 +96,8 @@ PINK = (230, 50, 230)
 d1 = 2
 d = Object(100, 200, 0.02, 1)
 d2 = Object(250, 200, 0.02, 1)
-d.P = (2*math.pi, 0.04)
-d2.P = (2*math.pi, 0.01)
+d.P = (0, 0.04)
+d2.P = (math.pi, 0.04)
 Objcts = Objects()
 Objcts.create_obj(d)
 Objcts.create_obj(d2)
@@ -102,7 +106,6 @@ while run:
     t += 0.0004
     win.fill(BLACK)
 
-    fall = (0, (g * t ** 2) / 2)
 
     Objcts.being(t, d, d2)
 
